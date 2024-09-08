9afe858aa6e51d23b8aaff01010b8bb91cbc9aa3
dshukertjr <dshukertjr@gmail.com>
2024-09-08 11:02:16 +0900

    feat: crud method tests pass

--- lib/src/mock_supabase_http_client.dart
+++ lib/src/mock_supabase_http_client.dart
@@ -8,19 +8,25 @@ class MockSupabaseHttpClient extends BaseClient {
   MockSupabaseHttpClient();
 
   void reset() {
+    // Clear the mock database
     _database.clear();
   }
 
   @override
   Future<StreamedResponse> send(BaseRequest request) async {
+    // Extract the table name from the URL
     final tableName = _extractTableName(request.url);
 
-    final body = request.method != 'GET' && request is Request
+    // Decode the request body if it's not a GET request
+    final body = (request.method != 'GET' && request.method != 'DELETE') &&
+            request is Request
         ? jsonDecode(await request.finalize().transform(utf8.decoder).join())
         : null;
 
+    // Handle different HTTP methods
     switch (request.method) {
       case 'POST':
+        // Handle upsert if the Prefer header is set
         final preferHeader = request.headers['Prefer'];
         if (preferHeader != null &&
             preferHeader.contains('resolution=merge-duplicates')) {
@@ -40,6 +46,7 @@ class MockSupabaseHttpClient extends BaseClient {
   }
 
   String _extractTableName(Uri url) {
+    // Extract the table name from the URL
     final pathSegments = url.pathSegments;
     final restIndex = pathSegments.indexOf('v1');
     if (restIndex != -1 && restIndex < pathSegments.length - 1) {
@@ -50,6 +57,7 @@ class MockSupabaseHttpClient extends BaseClient {
 
   StreamedResponse _handleInsert(
       String tableName, dynamic data, BaseRequest request) {
+    // Handle inserting data into the mock database
     if (data == null) {
       return _createResponse({'error': 'No data provided'},
           statusCode: 400, request: request);
@@ -67,6 +75,7 @@ class MockSupabaseHttpClient extends BaseClient {
 
   StreamedResponse _handleUpdate(
       String tableName, dynamic data, BaseRequest request) {
+    // Handle updating data in the mock database
     if (data == null) {
       return _createResponse({'error': 'No data provided'},
           statusCode: 400, request: request);
@@ -75,35 +84,68 @@ class MockSupabaseHttpClient extends BaseClient {
       return _createResponse({'error': 'Invalid data format'},
           statusCode: 400, request: request);
     }
-    final id = data['id'];
-    final index =
-        _database[tableName]?.indexWhere((item) => item['id'] == id) ?? -1;
-    if (index != -1) {
-      _database[tableName]![index] = {..._database[tableName]![index], ...data};
-      return _createResponse(_database[tableName]![index], request: request);
+
+    // Get query parameters for filtering
+    final queryParams = request.url.queryParameters;
+    var updated = false;
+
+    // Update items that match the filters
+    if (_database.containsKey(tableName)) {
+      for (var row in _database[tableName]!) {
+        if (_matchesFilters(row, queryParams)) {
+          row.addAll(data);
+          updated = true;
+        }
+      }
     }
-    return _createResponse({'error': 'Not found'},
-        statusCode: 404, request: request);
+
+    if (updated) {
+      return _createResponse(data, request: request);
+    } else {
+      return _createResponse({'error': 'Not found'},
+          statusCode: 404, request: request);
+    }
+  }
+
+  /// Checks if a given item matches the provided filters.
+  ///
+  /// This method iterates through each filter in the `filters` map,
+  /// parses the filter using `_parseFilter`, and applies it to the `item`.
+  /// If any filter doesn't match, the method returns false.
+  /// If all filters match, it returns true.
+  ///
+  /// [item] The item to check against the filters.
+  /// [filters] A map of filter keys and their corresponding values.
+  /// Returns true if the item matches all filters, false otherwise.
+  bool _matchesFilters(Map<String, dynamic> item, Map<String, String> filters) {
+    // Check if an item matches the provided filters
+    for (var key in filters.keys) {
+      final filter = _parseFilter(key, filters[key]!);
+      if (!filter(item)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   StreamedResponse _handleUpsert(
       String tableName, dynamic data, BaseRequest request) {
+    // Handle upserting data into the mock database
     if (data == null) {
       return _createResponse({'error': 'No data provided'},
-          statusCode: 400, request: request);
-    }
-    if (data is! List) {
-      return _createResponse({'error': 'Invalid data format'},
           statusCode: 400, request: request);
     }
     if (!_database.containsKey(tableName)) {
       _database[tableName] = [];
     }
 
-    final results = data.map((item) {
-      if (item is! Map<String, dynamic>) {
-        throw const FormatException('Invalid item format in upsert data');
-      }
+    // Convert data to a list of items
+    final List<Map<String, dynamic>> items = data is List
+        ? List<Map<String, dynamic>>.from(data)
+        : [Map<String, dynamic>.from(data)];
+
+    // Upsert each item
+    final results = items.map((item) {
       final id = item['id'];
       if (id != null) {
         final index =
@@ -125,21 +167,24 @@ class MockSupabaseHttpClient extends BaseClient {
 
   StreamedResponse _handleDelete(
       String tableName, dynamic data, BaseRequest request) {
-    if (data == null) {
-      return _createResponse({'error': 'No data provided'},
+    // Handle deleting data from the mock database
+    final queryParams = request.url.queryParameters;
+    if (queryParams.isEmpty) {
+      return _createResponse({'error': 'No query parameters provided'},
           statusCode: 400, request: request);
     }
-    if (data is! Map<String, dynamic>) {
-      return _createResponse({'error': 'Invalid data format'},
-          statusCode: 400, request: request);
+
+    if (_database.containsKey(tableName)) {
+      _database[tableName]!
+          .removeWhere((item) => _matchesFilters(item, queryParams));
     }
-    final id = data['id'];
-    _database[tableName]?.removeWhere((item) => item['id'] == id);
+
     return _createResponse({'message': 'Deleted'}, request: request);
   }
 
   StreamedResponse _handleSelect(
       String tableName, Map<String, String> queryParams, BaseRequest request) {
+    // Handle selecting data from the mock database
     if (!_database.containsKey(tableName)) {
       return _createResponse([], request: request);
     }
@@ -149,15 +194,42 @@ class MockSupabaseHttpClient extends BaseClient {
     // Handle basic filtering
     queryParams.forEach((key, value) {
       if (key != 'select') {
-        result = result.where((item) => item[key].toString() == value).toList();
+        final filter = _parseFilter(key, value);
+        result = result.where((item) => filter(item)).toList();
       }
     });
 
     return _createResponse(result, request: request);
   }
 
+  Function(Map<String, dynamic>) _parseFilter(String key, String value) {
+    // Parse filters from query parameters
+    if (key.contains('eq.')) {
+      final field = key.split('eq.')[1];
+      return (item) => item[field] == value;
+    } else if (key.contains('neq.')) {
+      final field = key.split('neq.')[1];
+      return (item) => item[field] != value;
+    } else if (key.contains('gte.')) {
+      final field = key.split('gte.')[1];
+      return (item) => item[field] >= num.tryParse(value);
+    } else if (key.contains('lte.')) {
+      final field = key.split('lte.')[1];
+      return (item) => item[field] <= num.tryParse(value);
+    } else if (key.contains('like.')) {
+      final field = key.split('like.')[1];
+      final regex = RegExp(value.replaceAll('%', '.*'));
+      return (item) => regex.hasMatch(item[field]);
+    } else if (key.contains('is.')) {
+      final field = key.split('is.')[1];
+      return (item) => item[field] == null;
+    }
+    return (item) => true;
+  }
+
   StreamedResponse _createResponse(dynamic data,
       {int statusCode = 200, required BaseRequest request}) {
+    // Create a response for the mock client
     return StreamedResponse(
       Stream.value(utf8.encode(jsonEncode(data))),
       statusCode,
--- test/mock_supabase_test.dart
+++ test/mock_supabase_test.dart
@@ -3,38 +3,94 @@ import 'package:supabase/supabase.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    late final SupabaseClient mockSupabase;
-    late final MockSupabaseHttpClient mockHttpClient;
-    setUpAll(() {
-      mockHttpClient = MockSupabaseHttpClient();
-      mockSupabase = SupabaseClient(
-        'https://mock.supabase.co',
-        'supabaseKey',
-        httpClient: mockHttpClient,
-      );
-    });
-    setUp(() {
-      // Additional setup goes here.
-    });
+  late final SupabaseClient mockSupabase;
+  late final MockSupabaseHttpClient mockHttpClient;
 
-    tearDown(() async {
-      // Reset the mock client after each test
-      mockHttpClient.reset();
-    });
+  setUpAll(() {
+    // Initialize the mock HTTP client and Supabase client
+    mockHttpClient = MockSupabaseHttpClient();
+    mockSupabase = SupabaseClient(
+      'https://mock.supabase.co',
+      'supabaseKey',
+      httpClient: mockHttpClient,
+    );
+  });
 
-    tearDownAll(() {
-      mockHttpClient.close();
-    });
+  setUp(() {
+    // Additional setup goes here.
+  });
 
-    test('First Test', () async {
+  tearDown(() async {
+    // Reset the mock client after each test
+    mockHttpClient.reset();
+  });
+
+  tearDownAll(() {
+    // Close the mock client after all tests
+    mockHttpClient.close();
+  });
+
+  group('basic CRUD tests', () {
+    test('Insert', () async {
+      // Test inserting a record
       await mockSupabase.from('posts').insert({'title': 'Hello, world!'});
       final posts = await mockSupabase.from('posts').select();
       expect(posts.length, 1);
       expect(posts.first, {'title': 'Hello, world!'});
     });
 
+    test('Upsert', () async {
+      // Test upserting a record
+      await mockSupabase
+          .from('posts')
+          .upsert({'id': 1, 'title': 'Initial post'});
+      await mockSupabase
+          .from('posts')
+          .upsert({'id': 1, 'title': 'Updated post'});
+      final posts = await mockSupabase.from('posts').select();
+      expect(posts.length, 1);
+      expect(posts.first, {'id': 1, 'title': 'Updated post'});
+    });
+
+    test('Update', () async {
+      // Test updating a record
+      await mockSupabase
+          .from('posts')
+          .insert({'id': 1, 'title': 'Original title'});
+      await mockSupabase
+          .from('posts')
+          .update({'title': 'Updated title'}).eq('id', 1);
+      final posts = await mockSupabase.from('posts').select();
+      expect(posts.length, 1);
+      expect(posts.first, {'id': 1, 'title': 'Updated title'});
+    });
+
+    test('Delete', () async {
+      // Test deleting a record
+      await mockSupabase
+          .from('posts')
+          .insert({'id': 1, 'title': 'To be deleted'});
+      await mockSupabase.from('posts').delete().eq('id', 1);
+      final posts = await mockSupabase.from('posts').select();
+      expect(posts.length, 0);
+    });
+
+    test('Select', () async {
+      // Test selecting records
+      await mockSupabase.from('posts').insert({'id': 1, 'title': 'First post'});
+      await mockSupabase
+          .from('posts')
+          .insert({'id': 2, 'title': 'Second post'});
+      final posts = await mockSupabase.from('posts').select().order('id');
+      expect(posts.length, 2);
+      expect(posts[0], {'id': 1, 'title': 'First post'});
+      expect(posts[1], {'id': 2, 'title': 'Second post'});
+    });
+  });
+
+  group('Filter tests', () {
     test('Filter by equality', () async {
+      // Test filtering by equality
       await mockSupabase.from('posts').insert({'title': 'Hello, world!'});
       final posts = await mockSupabase
           .from('posts')
@@ -45,6 +101,7 @@ void main() {
     });
 
     test('Filter by inequality', () async {
+      // Test filtering by inequality
       await mockSupabase.from('posts').insert({'title': 'Hello, world!'});
       final posts = await mockSupabase
           .from('posts')
@@ -55,6 +112,7 @@ void main() {
     });
 
     test('Filter by range', () async {
+      // Test filtering by range
       await mockSupabase.from('posts').insert({'id': 1, 'title': 'First post'});
       await mockSupabase
           .from('posts')
@@ -65,6 +123,7 @@ void main() {
     });
 
     test('Filter by like', () async {
+      // Test filtering by like
       await mockSupabase.from('posts').insert({'title': 'Hello, world!'});
       final posts =
           await mockSupabase.from('posts').select().like('title', '%world%');
@@ -73,6 +132,7 @@ void main() {
     });
 
     test('Filter by is null', () async {
+      // Test filtering by is null
       await mockSupabase.from('posts').insert({'title': null});
       final posts =
           await mockSupabase.from('posts').select().isFilter('title', null);
